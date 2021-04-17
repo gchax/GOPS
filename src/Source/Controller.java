8e6eca775a53773da294f75fbfdbcf126267e4a5
gchax <pcg.mega@gmail.com>
2021-04-17 19:09:48 +0700

    menu1

---
+++ src/Source/Controller.java
@@ -0,0 +1,35 @@
+package Source;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class Controller {
+
+    public Button startButton;
+    public Button tutorialButton;
+    public Button scoreButton;
+    public Button exitButton;
+
+    public void pressStart() throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Game.fxml")));
+        Stage window = (Stage) startButton.getScene().getWindow();
+        window.setScene(new Scene(root, 600, 400));
+    }
+
+    public void pressTutorial() {
+    }
+
+    public void pressScore() {
+    }
+
+    public void pressExit() {
+        System.exit(0);
+    }
+
+}
---
+++ src/Source/Main.java
@@ -0,0 +1,24 @@
+package Source;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage window) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Menu.fxml")));
+        window.setTitle("Hello World");
+        window.setScene(new Scene(root, 600, 400));
+        window.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
